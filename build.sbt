467d80775b46624e448cd4496ed04978a014a1be
Reynaldo <r.garcia@douglas.de>
2017-11-14 12:09:11 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "akka-stream-example"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
+
+// https://mvnrepository.com/artifact/com.typesafe.akka/akka-stream
+libraryDependencies += "com.typesafe.akka" % "akka-stream_2.12" % "2.5.6"
+
---
+++ src/main/scala/ej1.scala
@@ -0,0 +1,28 @@
+
+import akka.stream._
+import akka.stream.scaladsl._
+
+import akka.{ NotUsed, Done }
+import akka.actor.ActorSystem
+import akka.util.ByteString
+import scala.concurrent._
+import scala.concurrent.duration._
+import java.nio.file.Paths
+
+object Stream1 {
+
+  def main(args:Array[String]) = {
+
+    implicit val system = ActorSystem("QuickStart")
+    implicit val materializer = ActorMaterializer()
+
+    val source: Source[Int, NotUsed] = Source(1 to 100)
+
+    source.runForeach(i => println(i))(materializer)
+
+    val done: Future[Done] = source.runForeach(i => println(i))(materializer)
+
+    implicit val ec = system.dispatcher
+    done.onComplete(_ => system.terminate())
+  }
+}
